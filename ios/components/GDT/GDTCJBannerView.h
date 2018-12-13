509378aade064d0ca1506bf5d48f28829ed9a83b
chenjun <chenjun7878@qq.com>
2018-12-13 16:09:53 +0800

    新增GDT和TANV的部分代码

---
+++ ios/components/GDT/GDTCJBannerView.h
@@ -0,0 +1,16 @@
+//
+//  GDTCJBannerView.h
+//  RNCjAd
+//
+//  Created by 陈俊 on 2018/12/13.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTViewManager.h>
+
+@interface GDTCJBannerView : UIView
+
+@property (nonatomic, retain) GDTMobBannerView *bannerView;
+
+@end
